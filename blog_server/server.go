44ea4e9c5653fd3aeac45efda295379eda5e6fba
Kerem Dokumacı <kerem.dokumaci@yemeksepeti.com>
2021-12-06 23:53:37 +0300

    add: server's create blog method

--- blog_server/server.go
+++ blog_server/server.go
@@ -2,7 +2,9 @@ package blogserver
 
 import (
 	"context"
+	"fmt"
 	mongoclient "go-blog-service/blog_server/mongo_client"
+	"go-blog-service/blog_server/mongo_client/models"
 	"go-blog-service/blogpb"
 	"go-blog-service/utils"
 	"log"
@@ -10,15 +12,41 @@ import (
 	"strconv"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
+
+/****** CONSTANTS & UTILS *******/
+
+var errorChecker utils.ErrorChecker
 
 const (
 	port = 50051
 )
 
+/****** CONSTANTS & UTILS *******/
+
 type server struct{}
 
-var errorChecker utils.ErrorChecker
+func (*server) CreateBlog(ctx context.Context, req *blogpb.CreateBlogRequest) (res *blogpb.CreateBlogResponse, err error) {
+	blog := req.GetBlog()
+	data := models.Blog{
+		AuthorId: blog.GetAuthorId(),
+		Title:    blog.GetTitle(),
+		Content:  blog.GetContent(),
+	}
+
+	result, err := mongoclient.BlogCollection.InsertOne(context.TODO(), data)
+
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, fmt.Sprintf("Internal server error : %v", err))
+	}
+
+	blog.Id = result.InsertedID.(string)
+	return &blogpb.CreateBlogResponse{
+		Blog: blog,
+	}, nil
+}
 
 func StartServer(ctx context.Context) {
 	log.Printf("Starting Blog Server... Listening port %v", port)
