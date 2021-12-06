324f7cb35be14b71ae80da0361fddd21743d6ee4
Kerem Dokumacı <kerem.dokumaci@yemeksepeti.com>
2021-12-06 22:59:36 +0300

    add: os signal channel

--- main.go
+++ main.go
@@ -3,14 +3,17 @@ package main
 import (
 	"context"
 	blogserver "go-blog-service/blog_server"
+	"os"
+	"os/signal"
 )
 
 func main() {
-	ch := make(chan int)
+	ch := make(chan os.Signal, 1)
 	ctx := context.Background()
-	go func(channel chan int) {
-		blogserver.StartServer(ctx)
-	}(ch)
+	go func(c context.Context) {
+		blogserver.StartServer(c)
+	}(ctx)
 
+	signal.Notify(ch, os.Interrupt)
 	<-ch
 }
