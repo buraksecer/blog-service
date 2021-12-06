a98fefacc8ab455bc418e0b56ad569e3980a14fb
Kerem Dokumacı <kerem.dokumaci@yemeksepeti.com>
2021-12-06 22:51:58 +0300

    add: server start with routine

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"context"
+	blogserver "go-blog-service/blog_server"
+)
 
+func main() {
+	ch := make(chan int)
+	ctx := context.Background()
+	go func(channel chan int) {
+		blogserver.StartServer(ctx)
+	}(ch)
+
+	<-ch
 }
