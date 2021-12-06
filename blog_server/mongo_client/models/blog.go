54840fa98c594391cabe90d97083d01e3b77ec6b
Kerem Dokumacı <kerem.dokumaci@yemeksepeti.com>
2021-12-06 23:53:24 +0300

    update: Id field

--- blog_server/mongo_client/models/blog.go
+++ blog_server/mongo_client/models/blog.go
@@ -1,7 +1,7 @@
 package models
 
 type Blog struct {
-	Id       int    `bson:"_id,omitempty"`
+	Id       string `bson:"_id,omitempty"`
 	AuthorId string `bson:"author_id"`
 	Content  string `bson:"content"`
 	Title    string `bson:"title"`
