9580aef05a94b1d0ab108b549083603bce80ca06
Kerem Dokumacı <kerem.dokumaci@yemeksepeti.com>
2021-12-06 22:47:10 +0300

    add: server base operations

--- blog_server/server.go
+++ blog_server/server.go
@@ -1,5 +1,36 @@
 package blogserver
 
-func StartServer() {
+import (
+	"fmt"
+	"go-blog-service/blogpb"
+	"go-blog-service/utils"
+	"net"
+	"strconv"
 
+	"google.golang.org/grpc"
+)
+
+const (
+	port = 50051
+)
+
+type server struct{}
+
+var errorChecker utils.ErrorChecker
+
+func StartServer() {
+	errorChecker = utils.NewErrorChecker()
+	fmt.Printf("Starting Blog Server...\nListening port %v \n", port)
+	listener, err := net.Listen("tcp", "0.0.0.0:"+strconv.Itoa(port))
+
+	errorChecker.HasError(err).Fatal("")
+
+	opts := []grpc.ServerOption{}
+
+	s := grpc.NewServer(opts...)
+	blogpb.RegisterBlogServiceServer(s, &server{})
+
+	serveErr := s.Serve(listener)
+
+	errorChecker.HasError(serveErr).Fatal("")
 }
