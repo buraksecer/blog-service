ea61006ff63b558a29f8fb4d8f70d87c96aedb6d
Kerem Dokumacı <kerem.dokumaci@yemeksepeti.com>
2021-12-06 23:31:03 +0300

    add: close connection method

--- blog_server/mongo_client/mongo_client.go
+++ blog_server/mongo_client/mongo_client.go
@@ -12,6 +12,7 @@ import (
 var errorChecker utils.ErrorChecker
 
 var Client *mongo.Client
+var BlogCollection *mongo.Collection
 
 func StartMongoClient() {
 	log.Println("Connecting to MongoDB ...")
@@ -19,6 +20,14 @@ func StartMongoClient() {
 	errorChecker.HasError(err).Fatal("An error occured when connecting to Mongo")
 	connectionErr := client.Connect(context.TODO())
 	errorChecker.HasError(connectionErr).Fatal("An error occured when connecting to Mongo")
+	log.Println("Connected to MongoDB !")
 
 	Client = client
+	BlogCollection = client.Database("blog").Collection("blog")
+}
+
+//Run this func when program cancelled
+func CloseConnection() {
+	err := Client.Disconnect(context.TODO())
+	errorChecker.HasError(err).Fatal("An error occured when disconnecting from Mongo")
 }
