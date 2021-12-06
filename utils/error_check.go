9a5d7f6e00ab10f0c05850ade5bf9c85ebce564a
Kerem Dokumacı <kerem.dokumaci@yemeksepeti.com>
2021-12-06 22:46:43 +0300

    add: prefix option for fatal logs

--- utils/error_check.go
+++ utils/error_check.go
@@ -18,9 +18,14 @@ func (e ErrorChecker) HasError(err interface{}) ErrorChecker {
 	return e
 }
 
-func (e ErrorChecker) Fatal() {
+func (e ErrorChecker) Fatal(msg string) {
+	var message string
+	message = msg
+	if msg == "" {
+		message = "Fatal"
+	}
 	for err := range e.Errors {
-		log.Fatalf("Fatal Error : %v", err)
+		log.Fatalf("%v: %v", message, err)
 	}
 }
 
